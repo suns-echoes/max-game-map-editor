d1a36c9ac0e9c5ea7e9888f8700f045b55dfbaa8
Aneta Suns <suns.echoes@gmail.com>
2024-09-02 16:53:12 +0200

    Git pages, post 2

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -5,10 +5,28 @@ document.querySelectorAll('img[data-preview="true"]').forEach(function (image) {
 		const container = document.createElement('div');
 		container.className = 'image-preview-container';
 
-		const previewImage = image.parentElement.cloneNode(true);
-		previewImage.style.position = 'fixed';
+		const panel = document.createElement('div');
+		panel.className = 'log-panel';
+		container.appendChild(panel);
 
-		container.appendChild(previewImage);
+		if (image.title) {
+			const title = document.createElement('div');
+			title.className = 'label';
+			title.textContent = image.title;
+			panel.appendChild(title);
+		}
+
+		const grid = document.createElement('div');
+		grid.className = 'grid';
+		panel.appendChild(grid);
+
+		const bigImgWrapper = document.createElement('div');
+		grid.appendChild(bigImgWrapper);
+
+		const bigImage = document.createElement('img');
+		bigImage.src = image.src.replace('_small.', '.');
+		bigImgWrapper.appendChild(bigImage);
+
 		document.body.appendChild(container);
 
 		container.addEventListener('click', function (event) {
